331a4de3b486794869d758e666c8b0e4610cf9d7
Kevin Lin <developer@kevinlin.info>
2017-12-15 21:34:21 -0600

    Database initialization script

---
+++ orion/scripts/db_init.py
@@ -0,0 +1,18 @@
+"""
+This script is used to initialize the database for the first time by creating all necessary tables.
+"""
+
+from orion.models import BaseModel
+from orion.server import create_app
+
+
+def db_init():
+    """
+    Create an Orion application instance and create all tables defined by its models.
+    """
+    app = create_app()
+    BaseModel.metadata.create_all(bind=app.ctx.db.engine)
+
+
+if __name__ == '__main__':
+    db_init()
