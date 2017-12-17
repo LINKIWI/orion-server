793994c7bc0aec07510fd5433df046a54549d4bb
Kevin Lin <developer@kevinlin.info>
2017-12-17 07:11:41 -0600

    Users handler

---
+++ orion/handlers/users_handler.py
@@ -0,0 +1,28 @@
+import collections
+
+from orion.handlers.base_handler import BaseHandler
+from orion.models.location import Location
+
+
+class UsersHandler(BaseHandler):
+    """
+    Query all known users and associated devices.
+    """
+
+    methods = ['GET']
+    path = '/api/users'
+
+    def run(self, *args, **kwargs):
+        def reduction(acc, location):
+            acc[location.user].append(location.device)
+            return acc
+
+        locations = self.ctx.db.session.query(Location.user, Location.device).distinct().all()
+        user_devices = reduce(reduction, locations, collections.defaultdict(list))
+        # Reshape from a map of user -> device to an array containing objects with user and devices
+        formatted_user_devices = [
+            {'user': user, 'devices': devices}
+            for user, devices in user_devices.iteritems()
+        ]
+
+        return self.success(data=formatted_user_devices, status=200)
