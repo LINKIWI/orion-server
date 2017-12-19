3dca3b3bba03732d2a9413fb0e117e9f2cf07252
Kevin Lin <developer@kevinlin.info>
2017-12-19 12:50:28 -0600

    Main server module has application as variable

--- orion/server.py
+++ orion/server.py
@@ -2,12 +2,14 @@ import os
 
 from orion.app import create_app
 
+app = create_app()
+
 
 def main():
     """
     Run the application in a development environment.
     """
-    create_app().run(
+    app.run(
         host='0.0.0.0',
         port=int(os.environ.get('PORT', 5000)),
         threaded=True,
